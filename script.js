991125752a35182313358619ee78b88414d65a2e
aniket umbc <aniket.umbc@gmail.com>
2018-04-04 00:29:50 +0530

    object and inheritances

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-// var john ={
-//     name :'aniket',
-//     age:23,
-//     year:1983,
-//     job:'Software Engineer'
-// };
+var john ={
+    name :'aniket',
+    age:23,
+    year:1983,
+    job:'Software Engineer'
+};
 
-// //function constructor to create object blueprint 
+//function constructor to create object blueprint 
 
-// var Person = function(name,year,job,age){
-//     this.name = name; // this
-//     this.job = job;
-//     this.age = age;
-//     this.year = year;    
-// }
+var Person = function(name,year,job,age){
+    this.name = name; // this
+    this.job = job;
+    this.age = age;
+    this.year = year;    
+}
 
-// var john = new Person('aniket', 1995, 'Yahoo', 55);
-// var johny = new Person('aniket', 1965, 'Google', 55);
+var john = new Person('aniket', 1995, 'Yahoo', 55);
+var johny = new Person('aniket', 1965, 'Google', 55);
 
-// //console.log (john.calculateAge());
+//console.log (john.calculateAge());
 
-// console.log(john);
+console.log(john);
 
-// // inheritances 
-// Person.prototype.calculateAge = function(){
-//     console.log(2018 - this.year);
-// }
+// inheritances 
+Person.prototype.calculateAge = function(){
+    console.log(2018 - this.year);
+}
 
-// john.calculateAge();
-// johny.calculateAge() ;
+john.calculateAge();
+johny.calculateAge() ;
 
-// Object. create method 
+//Object. create method 
 
 var personProto = {
     calculateAge : function(){
